66c884e2f0db37dc3386e22c5aa9a17445c2159a
unknown <santiago.1701713773@ucaldas.edu.co>
2023-04-06 12:36:37 -0500

    Estrategias de autorizacion Parte 1

---
+++ src/config/configuracion.seguridad.ts
@@ -0,0 +1,9 @@
+export namespace ConfiguracionSeguridad {
+  export const menuProductoId = "640f77d8b31a5a36d0fb4837";
+  export const listarAccion = "listar";
+  export const guardarAccion = "guardar";
+  export const editarAccion = "editar";
+  export const eliminarAccion = "eliminar";
+  export const descargarAccion = "descargar";
+
+}

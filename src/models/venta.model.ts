9aebf84342e360e03103889252baf629621f25c9
unknown <santiago.1701713773@ucaldas.edu.co>
2023-04-06 08:12:38 -0500

    Cracion de controladores y relaciones entre modelos

--- src/models/venta.model.ts
+++ src/models/venta.model.ts
@@ -1,4 +1,7 @@
-import {Entity, model, property} from '@loopback/repository';
+import {Entity, belongsTo, hasMany, model, property} from '@loopback/repository';
+import {Cliente} from './cliente.model';
+import {Producto} from './producto.model';
+import {VentaProducto} from './venta-producto.model';
 
 @model()
 export class Venta extends Entity {
@@ -33,6 +36,17 @@ export class Venta extends Entity {
   })
   cantidadDisponible: number;
 
+  @property({
+    type: 'boolean',
+    required: false,
+  })
+  notificada?: number;
+
+  @belongsTo(() => Cliente)
+  clienteId: number;
+
+  @hasMany(() => Producto, {through: {model: () => VentaProducto}})
+  productos: Producto[];
 
   constructor(data?: Partial<Venta>) {
     super(data);

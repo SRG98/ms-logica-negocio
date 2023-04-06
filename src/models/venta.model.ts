36e4cc6cd01378a649d4bfb85c3c91f20b1490cb
unknown <santiago.1701713773@ucaldas.edu.co>
2023-04-06 08:00:34 -0500

    Creacion de modelos y repositorios, se hace migracion a la base de datos

---
+++ src/models/venta.model.ts
@@ -0,0 +1,46 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Venta extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  foto: string;
+
+  @property({
+    type: 'number',
+    required: false,
+  })
+  precioVenta: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  cantidadDisponible: number;
+
+
+  constructor(data?: Partial<Venta>) {
+    super(data);
+  }
+}
+
+export interface VentaRelations {
+  // describe navigational properties here
+}
+
+export type VentaWithRelations = Venta & VentaRelations;
